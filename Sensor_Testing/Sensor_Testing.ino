77833c755e83f5b4746af4d924be48e0b9585ef4
selinahsu <selinaraindrop@gmail.com>
2022-03-11 18:07:37 -0500

    accurately measures angle

--- Sensor_Testing/Sensor_Testing.ino
+++ Sensor_Testing/Sensor_Testing.ino
@@ -3,6 +3,7 @@
 
 #include <Wire.h>
 
+int16_t current_angle, delta_time, delta_angle, prev_time, curr_time, sample; 
 
 void setup()
 {
@@ -18,22 +19,26 @@ void setup()
 	}
 
 	initImu();
+  Serial.println("Setting starting angle to 0...");
+  current_angle = 0;
+  curr_time = millis();
 
 }
 
 void loop()
 {
 	//Read data then print
-	imu_data_t data = readAcceleration();
-	Serial.print("(");
-	Serial.print(data.x);
-	Serial.print(", ");
-	Serial.print(data.y);
-	Serial.print(", ");
-	Serial.print(data.z);
-	Serial.println(")");
-	delay(20);
+//	imu_data_t data = readAcceleration();
+//	Serial.print("(");
+//	Serial.print(data.x);
+//	Serial.print(", ");
+//	Serial.print(data.y);
+//	Serial.print(", ");
+//	Serial.print(data.z);
+//	Serial.println(")");
+//	delay(20);
 
+  Serial.println("Reading TOFs:");
 	for(int i = 0; i < SENSOR_COUNT; i++){
 		uint16_t data = readSensor((TOF_SENSOR)(SENSOR_1 + i));
 		Serial.print(data);
@@ -42,5 +47,15 @@ void loop()
 	}
 	Serial.println();
 
-	delay(1000);
+  Serial.println("Reading angle:");
+  prev_time = curr_time;
+  curr_time = millis();
+  imu_data_t data = readGyro();
+  sample = data.z;
+  delta_time = curr_time - prev_time;
+  delta_angle = sample*delta_time/1000;
+  current_angle += delta_angle;
+  Serial.println(current_angle);
+  Serial.println(" ");
+	delay(50);
 }
